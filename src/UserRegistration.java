620eb52b4994b16eadc9e074095f94b04e572866
asita26 <asita98890@gmail.com>
2023-12-06 21:32:51 +0530

    Adding code to take valid last name as input

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -2,20 +2,32 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegistration{
+public class UserRegistration {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        // Validate First Name
         System.out.println("Enter your first name: ");
         String firstName = scanner.nextLine();
 
         if (isValidFirstName(firstName)) {
-            System.out.println("Valid first name." + firstName );
+            System.out.println("Valid first name: " + firstName);
         } else {
             System.out.println("Invalid first name. Please make sure it starts with a capital letter and has a minimum of 3 characters.");
         }
+
+        // Validate Last Name
+        System.out.println("Enter your last name: ");
+        String lastName = scanner.nextLine();
+
+        if (isValidLastName(lastName)) {
+            System.out.println("Valid last name: " + lastName);
+        } else {
+            System.out.println("Invalid last name. Please make sure it starts with a capital letter and has a minimum of 3 characters.");
+        }
+
+        scanner.close();
     }
 
     private static boolean isValidFirstName(String firstName) {
@@ -31,4 +43,12 @@ public class UserRegistration{
         // Returning true if the matcher finds a match, indicating a valid first name
         return matcher.matches();
     }
+
+    private static boolean isValidLastName(String lastName) {
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
 }
+
